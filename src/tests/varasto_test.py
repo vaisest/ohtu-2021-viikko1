08cc5ffe2b217a9a680986a54904d50660f03290
vaisest <4550061+vaisest@users.noreply.github.com>
2021-11-05 17:34:50 +0200

    fix tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,29 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_virheellinen_tilavuus_ja_saldo(self):
+        vaara_varasto = Varasto(-10, alku_saldo=-10)
+        self.assertEqual(vaara_varasto.tilavuus, 0)
+        self.assertEqual(vaara_varasto.saldo, 0)
+
+    def test_saldo_enemmän_kuin_tilavuus(self):
+        vaara_varasto = Varasto(10, alku_saldo=20)
+        self.assertEqual(vaara_varasto.saldo, 10)
+
+    def test_lisaa_tyhja(self):
+        self.assertEqual(self.varasto.lisaa_varastoon(-10), None)
+
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(999)
+        self.assertEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_ota_tyhja(self):
+        self.assertEqual(self.varasto.ota_varastosta(-10), 0.0)
+
+    def test_ota_loput(self):
+        self.varasto.lisaa_varastoon(10)
+        self.assertEqual(self.varasto.ota_varastosta(999), 10)
+
+    def test_str(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
